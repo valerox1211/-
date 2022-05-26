0c7b858bb94ec9ad12a2ae300a8f831f4a59557e
valerox1211 <106289769+valerox1211@users.noreply.github.com>
2022-05-26 18:02:46 +0300

    Add files via upload
    Вывод информации об ученике c#

---
+++ tichers.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+namespace bogdanov
+{
+class Program
+{
+public static void Main(string[] args)
+{
+string uch = "Ученики"+ "[";
+
+
+Console.Write("Укажите код_номер ученика = ");
+int nomer = Convert.ToInt32(Console.ReadLine());
+
+string rom =  uch + nomer + "]";
+
+Console.Write(rom + ".ДАТА РОЖДЕНИЯ. ДЕНЬ = ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(rom + ".ДАТА РОЖДЕНИЯ. МЕСЯЦ = ");
+int month = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(rom + ".ДАТА РОЖДЕНИЯ. ГОД = ");
+int year = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(rom + "КЛАСС. = ");
+int klass = Convert.ToInt32(Console.ReadLine());
+
+
+}
+
+public static void Bag(string[] args){
+Console.ReadKey(true);
+}
+}
+}
